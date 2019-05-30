7794554c576c2f7d2c50e774461fb3e48f453892
ling <ljxiang1995@outlook.com>
2019-05-30 11:23:54 +0800

    新增122 so easy

---
+++ src/com/ling/leetCode/maxProfit_122/maxProfit.java
@@ -0,0 +1,25 @@
+package com.ling.leetCode.maxProfit_122;
+
+/**
+ * @ClassName maxProfit_122
+ * @Deacription TODO
+ * @Author ljxia
+ * @Date 2019/5/30 11:08
+ * @Version 1.0
+ **/
+public class maxProfit {
+    public int solution(int[] prices){
+        int sum = 0;
+        int length = prices.length;
+        if (length < 2){
+            return 0;
+        }
+        for (int i = 1; i < length; i++) {
+            int D_value = prices[i] - prices[i - 1];
+            if (D_value > 0){
+                sum+=D_value;
+            }
+        }
+        return sum;
+    }
+}
