b50b1e059f65fda9478b0e04d2e1f052eca553ed
ling <ljxiang1995@outlook.com>
2019-06-12 22:48:52 +0800

    Update GenerateParenthesis_22.java
    修正

--- src/com/ling/leetcode/middle/GenerateParenthesis_22.java
+++ src/com/ling/leetcode/middle/GenerateParenthesis_22.java
@@ -53,7 +53,7 @@ public class GenerateParenthesis_22 {
         //这里需要考虑一点，要是第一个方法已经存了str，那么后面那个千万不要进去了，不然又要存一次
         generate(list, n , left, right, str, true);
         //只允许执行一次
-        if (left == n && right > left){
+        if (left == n || right > left){
             return;
         }
         generate(list, n , left, right, str, false);
